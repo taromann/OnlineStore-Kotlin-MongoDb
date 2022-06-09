83a5a92064212c5ef33f96fb995a06e1a7fc478d
unknown <taskinroman@mail.ru>
2022-06-09 14:12:28 +0500

    made filtering

--- src/main/kotlin/com/github/assemblathe1/kotlinstore/repositories/ProductRepository.kt
+++ src/main/kotlin/com/github/assemblathe1/kotlinstore/repositories/ProductRepository.kt
@@ -3,6 +3,7 @@ package com.github.assemblathe1.kotlinstore.repositories
 import com.github.assemblathe1.kotlinstore.entities.Product
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.querydsl.QuerydslPredicateExecutor
 import org.springframework.stereotype.Repository
 
 @Repository
--- src/main/kotlin/com/github/assemblathe1/kotlinstore/services/ProductService.kt
+++ src/main/kotlin/com/github/assemblathe1/kotlinstore/services/ProductService.kt
@@ -1,22 +1,40 @@
 package com.github.assemblathe1.kotlinstore.services
 
-import com.github.assemblathe1.kotlinstore.dto.ProductDto
 import com.github.assemblathe1.kotlinstore.entities.Product
 import com.github.assemblathe1.kotlinstore.repositories.ProductRepository
 import org.bson.types.ObjectId
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.data.support.PageableExecutionUtils
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class ProductService @Autowired constructor(val productRepository: ProductRepository) {
+class ProductService @Autowired constructor(
+    val mongoTemplate: MongoTemplate,
+    val productRepository: ProductRepository
+) {
 
-    fun getAll(minPrice: Int?, maxPrice: Int?, titlePart: String?, page: Int): Page<Product?> {
-        // TODO specifications
-        return productRepository.findAll(PageRequest.of(page - 1, 10))
+    fun getAll(minPrice: Int?, maxPrice: Int?, titlePart: String?, page: Int): Page<Product?> /*List<Product>*/ {
+        val pageable = PageRequest.of(page - 1, 5)
+        val query = Query()
+        query.with(pageable)
+
+        titlePart?.let { query.addCriteria(Criteria.where("title").regex(titlePart)) }
+        val priceCriteria = Criteria("price")
+        minPrice?.let { priceCriteria.gte(minPrice) } ?: priceCriteria.gte(0)
+        maxPrice?.let { priceCriteria.lte(maxPrice) } ?: priceCriteria.lte(Int.MAX_VALUE)
+        query.addCriteria(priceCriteria)
+
+        return PageableExecutionUtils.getPage(
+            mongoTemplate.find(query, Product::class.java),
+            pageable
+        ) { mongoTemplate.count(query, Product::class.java) }
     }
 
     fun create(product: Product): Product = productRepository.insert(product)
